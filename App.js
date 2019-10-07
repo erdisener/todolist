33f0c4cb9d12ef582a13c0da827d4fa90ccad102
erdisener <erdisener@gmail.com>
2019-10-07 18:45:46 +0300

    Update App.js
    created text input and add button.

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="To Do..." 
+        style={styles.input}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection:'row', 
+    justifyContent: 'space-between', 
+    alignItems:'center'
+  },
+  input: {
+    width: '70%', 
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    padding: 10
+  }
 });
